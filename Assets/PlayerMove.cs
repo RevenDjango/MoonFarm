5edf86557b422b70984f7ed08ce51ef9f540c5bb
RevenDjango <datehortua5@gmail.com>
2022-01-05 20:24:04 -0500

    modificaciones scripts

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed = 0.5f;
+    [SerializeField] private float RotateSpeed = 200.0f;
+    [SerializeField] private float JumpForce = 200.0f;
+
+    void Start()
+    {
+        
+    }
+
+
+    private void Movement()
+    {
+
+    }
+}
